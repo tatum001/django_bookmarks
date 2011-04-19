81e243f90392c69c62ccbc8c13b8116c8157e99f
tatum <croisse@gmail.com>
2011-04-19 09:04:50 -0700

    django_bookmarks

--- django_bookmarks/bookmarks/views.py
+++ django_bookmarks/bookmarks/views.py
@@ -1,6 +1,8 @@
 #-*- coding:utf-8 -*-
 # Create your views here.
 from django.http import HttpResponse
+from django.template import Context
+from django.template.loader import get_template
 
 def main_page(request):
     template = get_template('main_page.html')
@@ -12,3 +14,21 @@ def main_page(request):
     output = template.render(variables)
     return HttpResponse(output)
     
+from django.http import HttpResponse, Http404
+from django.contrib.auth.models import User
+
+def user_page(request, username):
+    try:
+        user = User.objects.get(username=username)
+    except:
+        raise Http404('사용자를 찾을 수 없습니다')
+    
+    bookmarks = user.bookmark_set.all()
+    
+    template = get_template('user_page.html')
+    variables = Context({
+                         'username': username,
+                         'bookmarks': bookmarks
+    })
+    output = template.render(variables)
+    return HttpResponse(output)
--- django_bookmarks/urls.py
+++ django_bookmarks/urls.py
@@ -1,8 +1,15 @@
+import os.path 
 from django.conf.urls.defaults import *
 from bookmarks.views import *
-    
+
+site_media = os.path.join(
+    os.path.dirname(__file__), 'site_media'
+    ) 
+
 urlpatterns = patterns('',
-        (r'^$', main_page),    
+        (r'^$', main_page),
+        (r'^user/(\w+)/$', user_page), 
     )
+
 
 
