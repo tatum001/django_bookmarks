5d462063adf43a503ade763f3b10b7011d76496b
tatum <croisse@gmail.com>
2011-04-19 07:10:32 -0700

    django_bookmarks

--- django_bookmarks/settings.py
+++ django_bookmarks/settings.py
@@ -102,10 +102,14 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'django_bookmarks.urls'
 
+import os.path
+
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(os.path.dirname(__file__), 'templates'),
+    
 )
 
 INSTALLED_APPS = (
